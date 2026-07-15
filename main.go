8a2e79491097ecd9ee401e52621512e65ed1b4f4
deexth <hkabare@gmail.com>
2026-07-15 20:24:20 +0200

    fix: security check

--- main.go
+++ main.go
@@ -100,6 +100,6 @@ func main() {
 		ReadHeaderTimeout: 5 * time.Second,
 	}
 
-	// log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving on port: %s\n", strconv.Quote(port))
 	log.Fatal(srv.ListenAndServe())
 }
